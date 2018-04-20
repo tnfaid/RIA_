ba2432bb926b1b51db0de527c1b8dde56eeca838
tnfaid <nurfaidahtitik@gmail.com>
2018-04-20 22:13:20 +0700

    admin lte, and make few include part

---
+++ application/views/admin/home.php
@@ -0,0 +1,21 @@
+<?php include 'header.php'; ?>
+<?php include 'aside.php'; ?>
+<!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <h1>
+        Dashboard
+        <small>Control panel</small>
+      </h1>
+      <ol class="breadcrumb">
+        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+        <li class="active">Dashboard</li>
+      </ol>
+    </section>
+
+    <?php include 'content.php';?>
+  </div>
+<?php include 'footer.php';?>
+</body>
+</html>
